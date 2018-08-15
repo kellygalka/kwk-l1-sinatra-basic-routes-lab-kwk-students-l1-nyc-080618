4eea5a1d750d3aeaa13b4916458c0c9fbb652a2d
kellygalka <kelly3000a1@gmail.com>
2018-08-15 13:39:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@ class App < Sinatra::Base
 get '/name' do
   "My name is Kelly"
 end
+end
+class App < Sinatra::Base
 get'/hometown' do
   "My hometown is Whippany" do
 end
+end
+class App < Sinatra::Base
 get'/favorite-song' do
   "My favorite song is Back to you"
 end
