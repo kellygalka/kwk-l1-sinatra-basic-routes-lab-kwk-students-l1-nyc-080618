16a56d4a59527bd599e3263380ea7a386a221b22
kellygalka <kelly3000a1@gmail.com>
2018-08-15 13:35:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 require_relative 'config/environment'
-get '/' do
-  "name"
-end
 class App < Sinatra::Base
+get '/name' do
+  "Kelly"
+end
+get'/hometown' do
+  "Whippany" do
+end
+get'/favorite song' do
+  "Back to you"
+end
 end
